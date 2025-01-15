ea42c9457b073555487d526b7ab2cc6e9fcfeca2
yek-j <jeonyk219@gmail.com>
2025-01-15 21:34:17 +0900

    timer button icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@
 
 import { displayTime } from "@/components/timer";
 import { useState } from "react";
+import { PlayIcon, PauseIcon, ArrowPathIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
-  const [time, setTime] = useState(1500)
+  const [time, setTime] = useState(1500);
+  const [isRunning, setIsRunning] = useState(false);
 
   const addFiveMin = () => {
     if ((time + 300) > 3600) setTime(3600);
@@ -19,21 +21,46 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 space-y-8">
-      <div className="flex flex-col items-center space-y-8 text-8xl font-bold">
+      {/** 타이머 부분 */}
+      <div className="text-8xl font-bold">
         {displayTime(time)}
       </div>
-      <div className="flex space-x-4">
-        <button 
-          className="px-4 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600 transition-colors"
-          onClick={addFiveMin}
-        >
-          + 5분
-        </button>
-        <button 
-          className="px-4 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600 transition-colors"
-          onClick={subFiveMin}>
-            - 5분
-        </button>
+
+      <div className="flex items-center space-x-8">
+        {/** 타이머 컨트롤 버튼  */}
+        <div className="flex space-x-4">
+          <button
+            className="p-2 rounded-full hover:bg-gray-100 transition-colors"
+            onClick={()=>setIsRunning(!isRunning)}
+          >
+            {isRunning ? (
+              <PauseIcon className="h-8 w-8 text-sky-700"/>
+            ) : (
+              <PlayIcon className="h-8 w-8 text-sky-700"/>
+            )}
+          </button>
+          <button>
+            <ArrowPathIcon className="h-8 w-8 text-sky-700"/>
+          </button>
+        </div>
+
+        {/** 시간 변경 버튼 */}
+        <div className="flex space-x-4">
+          <button 
+            className="px-4 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600 transition-colors"
+            onClick={addFiveMin} 
+            disabled={isRunning}
+          >
+            + 5분
+          </button>
+          <button 
+            className="px-4 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600 transition-colors"
+            onClick={subFiveMin}
+            disabled={isRunning}
+          >
+              - 5분
+          </button>
+        </div>
       </div>
     </div>
   );
